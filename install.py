ac9d98ed703e2d4ee5732950dc94fc18a4b0366c
trig0n <pascal@eberlein.io>
2019-01-01 23:58:32 +0100

    update

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 from os import getcwd, geteuid
+from sys import argv
+from loguru import logger as log
 
 
 class Setup(object):
@@ -20,18 +22,22 @@ class Setup(object):
 
     @staticmethod
     def _install_autostart():
+        log.info("installing to autostart")
         with open(Setup.AUTOSTART_PATH, 'a') as o:
+            # o.write("@lxterminal -e cd " + getcwd() + "/; git pull")
             o.write("@lxterminal -e /usr/bin/sudo /usr/bin/python3 " + getcwd() + "/" + Setup.SCRIPT)
         return True
 
     @staticmethod
     def install_autostart():
         if Setup.already_autostart_installed():
+            log.info("autostart already installed")
             return True
         Setup._install_autostart()
 
     @staticmethod
     def _uninstall_autostart():
+        log.info("uninstalling autostart")
         _o = Setup.read_autostart_file()
         with open(Setup.AUTOSTART_PATH, 'w') as o:
             for l in _o:
@@ -41,20 +47,22 @@ class Setup(object):
     @staticmethod
     def uninstall_autostart():
         if not Setup.already_autostart_installed():
+            log.info("autostart already not enabled")
             return True
         Setup._uninstall_autostart()
 
     @staticmethod
     def help():
-        print("usage: python3 carpi.py {arguments}")
-        print("{arguments}:")
-        print("\t-i\t--install")
-        print("\t-u\t--uninstall")
+        log.info("usage: python3 carpi.py {arguments}")
+        log.info("{arguments}:")
+        log.info("\t-i\t--install")
+        log.info("\t-u\t--uninstall")
         exit()
 
     @staticmethod
     def parse_arguments(arguments):
         i = 0
+        log.info("parsing args")
         while i < len(arguments):
             a = arguments[i]
             if a in ["-i", "--install"]:
@@ -67,11 +75,15 @@ class Setup(object):
 
 def should_not_be_root():
     if geteuid() == 0:
-        print("should not modify", Setup.AUTOSTART_PATH, "with root permissions")
+        log.error("should not modify", Setup.AUTOSTART_PATH, "with root permissions")
         exit()
 
 
 def should_be_root():
     if geteuid() != 0:
-        print("need root for apt calls and display driver install")
+        log.error("need root for apt calls and display driver install")
         exit()
+
+
+if __name__ == '__main__':
+    Setup.parse_arguments(argv)
