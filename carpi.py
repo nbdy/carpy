3bebc8c6bef753fe7beb8c7451beccfaf536adb1
trig0n <pascal@eberlein.io>
2019-01-12 20:59:24 +0100

    update

--- carpi.py
+++ carpi.py
@@ -437,10 +437,10 @@ if __name__ == '__main__':
 
     if Static.is_pi():
         log.debug("is pi, setting fullscreen")
-        # Config.set("graphics", "fullscreen", 0)
-        Config.set("graphics", "left", 0)
-        Config.set("graphics", "top", 0)
-        Config.set("graphics", "position", "custom")
+        Config.set("graphics", "fullscreen", 1)
+        # Config.set("graphics", "left", 0)
+        # Config.set("graphics", "top", 0)
+        # Config.set("graphics", "position", "custom")
         Config.set("graphics", "height", 480)
         Config.set("graphics", "width", 320)
     else:
